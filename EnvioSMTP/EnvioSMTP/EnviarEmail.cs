b2ae09d6fb4ab1bbbef62d7bedcd19101ebbe84b
Joylson <joylsont@gmail.com>
2018-03-30 20:25:04 -0300

    Inserindo Projeto

---
+++ EnvioSMTP/EnvioSMTP/EnviarEmail.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Net;
+using System.Net.Mail;
+
+namespace EnvioSMTP
+{
+    public class EnviarEmail
+    {
+        public static void EnviarMessagemEmail(string from, string to, string subject, string messagem)
+        {
+            var Email = new MailMessage();
+            Email.To.Add(new MailAddress(to));
+            Email.From = new MailAddress(from);
+            Email.Subject = subject;
+            Email.IsBodyHtml = true;
+            Email.Body = messagem;
+            var SMTPClient = new SmtpClient("smtp.gmail.com", 587);
+            SMTPClient.EnableSsl = true;
+            SMTPClient.UseDefaultCredentials = false;
+            SMTPClient.Credentials = new NetworkCredential("SEU EMAIL", "SUA SENHA");
+            SMTPClient.Send(Email);
+        }
+    }
+}
---
+++ EnvioSMTP/EnvioSMTP/Teste.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EnvioSMTP
+{
+    public class Teste
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                EnviarEmail.EnviarMessagemEmail(
+                    "REMETENTE",
+                    "DESTINARIO",
+                    "TITULO",
+                    "MENSAGEM EM HTML"
+                    );
+                Console.WriteLine("CONCLUIDO");
+            }
+            catch
+            {
+                Console.WriteLine("ERRO");
+            }
+            Console.WriteLine("Aperta qualquer tecla para finalizar");
+            Console.ReadKey();
+        }
+    }
+}
+}
